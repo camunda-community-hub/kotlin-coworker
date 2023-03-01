35bac71182d8f698cccb528b7434378d5a1f2b45
Alexey Vinogradov <vinogradov.a.i.93@gmail.com>
2023-03-01 18:32:09 +0500

    fix(): make interceptors and call credentials work
    There is a speciality with configuration - you should put interceptors and a `CallOptions` object directly in the constructor.

---
+++ coworker-core/src/main/kotlin/org/camunda/community/extension/coworker/credentials/ZeebeCoworkerClientCredentials.kt
@@ -0,0 +1,36 @@
+package org.camunda.community.extension.coworker.credentials
+
+import io.camunda.zeebe.client.CredentialsProvider
+import io.grpc.CallCredentials
+import io.grpc.Metadata
+import io.grpc.SecurityLevel
+import io.grpc.Status
+import mu.KLogging
+import java.io.IOException
+import java.util.concurrent.Executor
+
+class ZeebeCoworkerClientCredentials(
+    private val credentialsProvider: CredentialsProvider
+) : CallCredentials() {
+    override fun applyRequestMetadata(requestInfo: RequestInfo, appExecutor: Executor, applier: MetadataApplier) {
+        if (requestInfo.securityLevel.ordinal < SecurityLevel.PRIVACY_AND_INTEGRITY.ordinal) {
+            logger.warn {
+                "The request's security level does not guarantee that the credentials will be confidential."
+            }
+        }
+
+        val headers = Metadata()
+        appExecutor.execute {
+            try {
+                credentialsProvider.applyCredentials(headers)
+                applier.apply(headers)
+            } catch (e: IOException) {
+                applier.fail(Status.CANCELLED.withCause(e))
+            }
+        }
+    }
+
+    override fun thisUsesUnstableApi() = Unit
+
+    companion object : KLogging()
+}
